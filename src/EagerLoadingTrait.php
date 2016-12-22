a26bcb3b8bb355b4de9fe0cc5eb520f6d5586519
Omer Mohamed <blinness@gmail.com>
2016-12-22 16:09:31 +0100

    Fix eager loading trait for Phalcon 3.0

--- src/EagerLoadingTrait.php
+++ src/EagerLoadingTrait.php
@@ -2,8 +2,8 @@
 
 use Sb\Framework\Mvc\Model\EagerLoading\Loader;
 
-trait EagerLoadingTrait
-{
+trait EagerLoadingTrait {
+
     /**
      * <code>
      * <?php
@@ -13,7 +13,7 @@ trait EagerLoadingTrait
      *
      * $manufacturers = Manufacturer::with('Robots.Parts', [
      *     'limit' => [$limit, $offset]
-     * ]);
+     * ]);  
      *
      * foreach ($manufacturers as $manufacturer) {
      *     foreach ($manufacturer->robots as $robot) {
@@ -24,68 +24,77 @@ trait EagerLoadingTrait
      * </code>
      *
      * @param mixed ...$arguments
-     * @return \Phalcon\Mvc\ModelInterface[]
+     * @return Phalcon\Mvc\ModelInterface[]
      */
-    public static function with()
-    {
-        $arguments = func_get_args();
-        if (!empty($arguments)) {
+    static public function with(...$arguments) {
+        if (! empty ($arguments)) {
             $numArgs    = count($arguments);
             $lastArg    = $numArgs - 1;
-            $parameters = null;
+            $parameters = NULL;
+
             if ($numArgs >= 2 && is_array($arguments[$lastArg])) {
                 $parameters = $arguments[$lastArg];
-                unset($arguments[$lastArg]);
-                if (isset($parameters['columns'])) {
+                
+                unset ($arguments[$lastArg]);
+
+                if (isset ($parameters['columns'])) {
                     throw new \LogicException('Results from database must be full models, do not use `columns` key');
                 }
             }
-        } else {
+        }
+        else {
             throw new \BadMethodCallException(sprintf('%s requires at least one argument', __METHOD__));
         }
+
         $ret = static::find($parameters);
+
         if ($ret->count()) {
-            array_unshift($arguments, $ret);
-            $ret = call_user_func_array('Phalcon\Mvc\Model\EagerLoading\Loader::fromResultset', $arguments);
+            $ret = Loader::fromResultset($ret, ...$arguments);
         }
+
         return $ret;
     }
+
     /**
      * Same as EagerLoadingTrait::with() for a single record
      *
      * @param mixed ...$arguments
-     * @return false|\Phalcon\Mvc\ModelInterface
+     * @return false|Phalcon\Mvc\ModelInterface
      */
-    public static function findFirstWith()
-    {
-        $arguments = func_get_args();
-        if (!empty($arguments)) {
+    static public function findFirstWith(...$arguments) {
+        if (! empty ($arguments)) {
             $numArgs    = count($arguments);
             $lastArg    = $numArgs - 1;
-            $parameters = null;
+            $parameters = NULL;
+
             if ($numArgs >= 2 && is_array($arguments[$lastArg])) {
                 $parameters = $arguments[$lastArg];
-                unset($arguments[$lastArg]);
-                if (isset($parameters['columns'])) {
+                
+                unset ($arguments[$lastArg]);
+
+                if (isset ($parameters['columns'])) {
                     throw new \LogicException('Results from database must be full models, do not use `columns` key');
                 }
             }
-        } else {
+        }
+        else {
             throw new \BadMethodCallException(sprintf('%s requires at least one argument', __METHOD__));
         }
+
         if ($ret = static::findFirst($parameters)) {
-            array_unshift($arguments, $ret);
-            $ret = call_user_func_array('Phalcon\Mvc\Model\EagerLoading\Loader::fromModel', $arguments);
+            $ret = Loader::fromModel($ret, ...$arguments);
         }
+
         return $ret;
     }
+
     /**
      * <code>
      * <?php
      *
      * $manufacturer = Manufacturer::findFirstById(51);
      *
-     * $manufacturer->load('Robots.Parts');
+     * $manufacturer->load('Robots.Parts'); 
      *
      * foreach ($manufacturer->robots as $robot) {
      *    foreach ($robot->parts as $part) { ... }
@@ -95,10 +104,7 @@ trait EagerLoadingTrait
      * @param mixed ...$arguments
      * @return self
      */
-    public function load()
-    {
-        $arguments = func_get_args();
-        array_unshift($arguments, $this);
-        return call_user_func_array('Phalcon\Mvc\Model\EagerLoading\Loader::fromModel', $arguments);
+    public function load(...$arguments) {
+        return Loader::fromModel($this, ...$arguments);
     }
 }
